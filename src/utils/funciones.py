74f4c85e8c21cfb5877acb4282aa2d879b311281
Gregorio Benavidez <geonobena@gmail.com>
2025-04-30 13:03:55 -0300

    creacion main_individuos.ipynb, primera actualizacion, creacion del dataset individuos

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -1,16 +1,15 @@
-from constantes import DATA_RAW_PATH , DATA_PROCESSED_PATH
 from glob import glob 
+from src.utils.constantes import DATA_PROCESSED_PATH, DATA_RAW_PATH
 
 def unir_lineas(f,processed):
     for lines in f: 
         processed.write(lines)
 
-def unir_archivos_individuos (tipo): 
+def unir_archivos (tipo): 
     new_tipo = str(tipo) + "*"
     new_tipo_csv = str(tipo) + ".csv"
     encabezado = False
     archivo_processed = DATA_PROCESSED_PATH / new_tipo_csv
-    
     with archivo_processed.open("w") as processed:
         
         for archivo in DATA_RAW_PATH.glob(new_tipo):
